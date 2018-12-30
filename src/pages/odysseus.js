ba4a5bee364286055dd49ce783d5eff2e43283ae
Ian Wright <wright.ianb@gmail.com>
2018-12-30 13:48:02 -0700

    add axes to odysseus

--- src/pages/odysseus.js
+++ src/pages/odysseus.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
+import { AxisBottom, AxisLeft } from '@vx/axis'
 import { Group } from '@vx/group'
 import { Bar } from '@vx/shape'
 import { scaleLinear, scaleBand } from '@vx/scale'
@@ -20,9 +21,9 @@ export default ({ data }) => {
     .map(([key, value]) => ({ color: key, value }))
     .sort((a, b) => a.value > b.value)
 
-  const width = 500
+  const width = 750
   const height = 500
-  const margin = { top: 20, bottom: 20, left: 20, right: 20 }
+  const margin = { top: 50, bottom: 50, left: 50, right: 50 }
 
   const xMax = width - margin.left - margin.right
   const yMax = height - margin.top - margin.bottom
@@ -38,6 +39,7 @@ export default ({ data }) => {
   const yScale = scaleLinear({
     rangeRound: [yMax, 0],
     domain: [0, Math.max(...frequencies.map(y))],
+    nice: true,
   })
 
   const compose = (scale, accessor) => frequencies =>
@@ -55,22 +57,40 @@ export default ({ data }) => {
     <Layout>
       <SEO title="" />
       <h1>Odysseus</h1>
-      <Link to="/">Home</Link>
       <svg width={width} height={height}>
-        {frequencies.map((d, i) => {
-          const barHeight = yMax - yPoint(d)
-          return (
-            <Group key={`bar-${i}`}>
-              <Bar
-                x={xPoint(d)}
-                y={yMax - barHeight}
-                height={barHeight}
-                width={xScale.bandwidth()}
-                fill="#fc2e1c"
-              />
-            </Group>
-          )
-        })}
+        <Group>
+          <AxisBottom
+            top={yMax + margin.top}
+            left={margin.left}
+            scale={xScale}
+            hideAxisLine
+            hideTicks
+          />
+          <AxisLeft
+            scale={yScale}
+            top={margin.top}
+            left={margin.left}
+            numTicks={5}
+            hideAxisLine
+            hideTicks
+            hideZero
+          />
+          {frequencies.map((d, i) => {
+            const barHeight = yMax - yPoint(d)
+            return (
+              <Group left={margin.left} top={margin.top} key={`bar-${i}`}>
+                <Bar
+                  x={xPoint(d)}
+                  y={yMax - barHeight}
+                  height={barHeight}
+                  width={xScale.bandwidth()}
+                  fill="#fc2e1c"
+                  stroke="black"
+                />
+              </Group>
+            )
+          })}
+        </Group>
       </svg>
     </Layout>
   )
