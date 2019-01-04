1927a470409f1bf2c3f1d2b0127171164cd4ed00
Ian Wright <wright.ianb@gmail.com>
2019-01-04 13:38:56 -0700

    remove static odysseus page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -8,7 +7,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
     <h1>Home</h1>
-    <Link to="/odysseus/">Odysseus</Link>
   </Layout>
 )
 
