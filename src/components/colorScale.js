88d033b9c949293f9e9fa147d2f56db59a5d9964
Ian Wright <wright.ianb@gmail.com>
2019-01-04 15:35:47 -0700

    create ColorScale component

---
+++ src/components/colorScale.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+import { AxisBottom, AxisLeft } from '@vx/axis'
+import { Group } from '@vx/group'
+import { Bar } from '@vx/shape'
+import { scaleLinear, scaleBand } from '@vx/scale'
+
+import styles from './colorScale.module.css'
+
+const ColorScale = ({ frequencies }) => {
+  const width = 750
+  const height = 500
+  const margin = { top: 50, bottom: 50, left: 50, right: 50 }
+
+  const xMax = width - margin.left - margin.right
+  const yMax = height - margin.top - margin.bottom
+
+  const x = d => d.color
+  const y = d => d.value
+
+  const xScale = scaleBand({
+    rangeRound: [0, xMax],
+    domain: frequencies.map(x),
+    padding: 0.4,
+  })
+  const yScale = scaleLinear({
+    rangeRound: [yMax, 0],
+    domain: [0, Math.max(...frequencies.map(y))],
+    nice: true,
+  })
+
+  const compose = (scale, accessor) => frequencies =>
+    scale(accessor(frequencies))
+  const xPoint = compose(
+    xScale,
+    x
+  )
+  const yPoint = compose(
+    yScale,
+    y
+  )
+
+  return (
+    <svg width={width} height={height}>
+      <Group>
+        <AxisBottom
+          top={yMax + margin.top}
+          left={margin.left}
+          scale={xScale}
+          hideAxisLine
+          hideTicks
+        />
+        <AxisLeft
+          scale={yScale}
+          top={margin.top}
+          left={margin.left}
+          numTicks={5}
+          hideAxisLine
+          hideTicks
+          hideZero
+        />
+        {frequencies.map((d, i) => {
+          const barHeight = yMax - yPoint(d)
+          return (
+            <Group left={margin.left} top={margin.top} key={`bar-${i}`}>
+              <Bar
+                className={styles[d.color.toLowerCase()]}
+                x={xPoint(d)}
+                y={yMax - barHeight}
+                height={barHeight}
+                width={xScale.bandwidth()}
+                stroke="black"
+              />
+            </Group>
+          )
+        })}
+      </Group>
+    </svg>
+  )
+}
+
+export default ColorScale
