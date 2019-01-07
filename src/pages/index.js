a20f31bc71117230ffb621cba3b80cedf9c73df3
Ian Wright <wright.ianb@gmail.com>
2019-01-07 15:37:33 -0700

    add location and theme links

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,32 @@ export default ({ data }) => {
   const characters = Array.from(
     new Set(edges.map(({ node }) => node.character.toLowerCase()))
   )
+  const locations = Array.from(
+    new Set(edges.map(({ node }) => node.location.toLowerCase()))
+  )
+  const themes = Array.from(
+    new Set(edges.map(({ node }) => node.theme.toLowerCase()))
+  )
   return (
     <Layout>
       <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
       <h1>Home</h1>
       <h2>Characters</h2>
       {characters.map(character => (
-        <div id={`${character}-link`}>
+        <div key={`${character}-link`}>
           <Link to={`characters/${character}`}>{character}</Link>
+        </div>
+      ))}
+      <h2>Locations</h2>
+      {locations.map(location => (
+        <div key={`${location}-link`}>
+          <Link to={`locations/${location}`}>{location}</Link>
+        </div>
+      ))}
+      <h2>Themes</h2>
+      {themes.map(theme => (
+        <div key={`${theme}-link`}>
+          <Link to={`themes/${theme}`}>{theme}</Link>
         </div>
       ))}
     </Layout>
@@ -28,6 +46,8 @@ export const query = graphql`
       edges {
         node {
           character
+          location
+          theme
         }
       }
     }
