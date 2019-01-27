3f73107f10811e54728963fe8ab3bc426c58d57f
Ian Wright <wright.ianb@gmail.com>
2019-01-26 17:33:13 -0700

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-    {
       resolve: 'gatsby-source-airtable',
       options: {
         tables: [
