94cde977c9ae135907960f8cc041b5edbe477507
Ian Wright <wright.ianb@gmail.com>
2019-03-01 11:47:41 -0700

    stub out odysseus in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,40 @@ import SEO from '../components/seo'
 
 export default ({ data }) => {
   const { edges } = data.allAirtable
-  const characters = Array.from(
-    new Set(
-      edges
-        .filter(({ node: { data } }) => data.character)
-        .map(({ node: { data } }) => data.character.toLowerCase())
-    )
-  ).sort((a, b) => a.localeCompare(b))
+  // const characters = Array.from(
+  //   new Set(
+  //     edges
+  //       .filter(({ node: { data } }) => data.character)
+  //       .map(({ node: { data } }) => data.character.toLowerCase())
+  //   )
+  // ).sort((a, b) => a.localeCompare(b))
+  //
+  const colorsByCharacter = {}
+  for (const {
+    node: {
+      data: { character, color },
+    },
+  } of edges) {
+    if (!(character in colorsByCharacter)) {
+      colorsByCharacter[character] = {}
+    }
+    if (!(color in colorsByCharacter[character])) {
+      colorsByCharacter[character][color] = 1
+    } else {
+      colorsByCharacter[character][color] =
+        colorsByCharacter[character][color] + 1
+    }
+  }
+
+  console.log(colorsByCharacter)
+
   return (
     <Layout>
       <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
       <h2>Characters</h2>
       <div>
-        {characters.map(character => (
-          <div key={character}>
-            <div>{JSON.stringify(character)}</div>
-          </div>
-        ))}
+        <h3>Odysseus</h3>
+        <div>{JSON.stringify(colorsByCharacter.Odysseus)}</div>
       </div>
     </Layout>
   )
@@ -33,11 +50,8 @@ export const query = graphql`
       edges {
         node {
           data {
-            book
             character
             color
-            location
-            theme
           }
         }
       }
